ee33370a5e3d3dfab7122dacfc5a7e1cc4dc33bd
root <marko.hinojosa@gmail.com>
2017-08-10 00:15:00 -0500

    Cambio prueba para sibir en linea de comandos

--- java-numberGenerator/src/test/java/mx/com/softland/AppTest.java
+++ java-numberGenerator/src/test/java/mx/com/softland/AppTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class AppTest {
 
 	@Test
-	public void testLengthOfTheUniqueKey() {
+	public void testLengthOfTheUniqueKey() {  
 
 		App obj = new App();
 		Assert.assertEquals(36, obj.generateUniqueKey().length());
