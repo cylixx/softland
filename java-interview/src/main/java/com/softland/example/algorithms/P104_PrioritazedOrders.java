72c7bdd4fae09f50e498f3dec8d84de7cddb4f20
root <marko.hinojosa@gmail.com>
2019-07-03 15:46:36 -0500

    update

---
+++ java-interview/src/main/java/com/softland/example/algorithms/P104_PrioritazedOrders.java
@@ -0,0 +1,24 @@
+package com.softland.example.algorithms;
+
+import java.util.List;
+
+public class P104_PrioritazedOrders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*
+	//All orders (prime or otherwise) - alphanumeric ID
+	 * Prime order - alphanumericID + aditional metadata (space delimited of lowercase)
+	 * Non-prime order - alphanumericID + aditional metadata (space delimited of positive integrers)
+	*/
+	static public List<String> prioritizedOrders(int numOrders, List<String> orderList) {
+		
+		
+		return null;
+	}
+	
+	
+}
